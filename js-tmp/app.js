5b2f8be7df1bc9ec83a7b8dbf099a04ea62ed0f4
vroa74 <33233830+vroa74@users.noreply.github.com>
2025-10-20 14:43:15 -0600

    up003

--- js-tmp/app.js
+++ js-tmp/app.js
@@ -9,5 +9,20 @@ if(true){
 
 }
 console.log(nombre);
+console.log('-------------------------------------------------------');
 
 // capitulo 3 funciones flecha
+
+function nombrex(){
+    console.log('victor');
+}
+
+nombrex();
+// funcion flecha
+const nombrexxx = () => {
+    return 'victor desde de funcion flecha'
+}
+
+let nombrexx= nombrexxx();
+
+console.log(nombrexx);
