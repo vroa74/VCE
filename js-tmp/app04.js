62810d191156dddea2eed20bda8e0d56b84b941d
vroa74 <33233830+vroa74@users.noreply.github.com>
2025-10-20 14:49:02 -0600

    up0004

---
+++ js-tmp/app04.js
@@ -0,0 +1,8 @@
+let nombre = "victor roman ";
+let apa= "ortiz";
+let ama="abreu";
+let edad= 51;
+
+let texto = `Hola mi nombre es ${nombre} ${apa} ${ama} y mi edad es ${edad}`
+
+console.log(texto);
